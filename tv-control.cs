badcd88553aefc9e6fcc23869f2e588e76b7bee3
Mehdi Hajati <mehdimohamad6@gmail.com>
2023-11-09 14:47:25 +0330

    I edited the app for reducing users problem like entering yesss

--- tv-control.cs
+++ tv-control.cs
@@ -12,17 +12,19 @@ namespace Desktop
             //!ask did you like it?
             //! if you liked it contenue the app
             //! if you didnt like it breaks the loop
-            var channelName = 1;
+            var channelName = 0;
             var likeIt =false ;
             do{
-               Console.WriteLine($"channel {channelName++}");
+               Console.WriteLine($"channel {++channelName}");
                Console.WriteLine($"do you love this channel ? (yes/no)");
+               Console.WriteLine("1) Yes");
+               Console.WriteLine("2) No");
                var answer = Console.ReadLine();
-               likeIt = answer == "yes" ? true : false ;
+               likeIt = answer == "1" ? true : false ;
             }while(!likeIt);
+            Console.Clear();
             Console.WriteLine($"Enjoy Wtching channel {channelName}");
             Console.WriteLine($"Good Bye");
-            Console.Clear();
         }
 
     }
